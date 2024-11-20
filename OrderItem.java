aa037a4d6d8bb10b28c617aa7302cafd77bdcafa
SinhaWiz <sinhartx2070@gmail.com>
2024-11-20 15:42:28 +0600

    added method for price calculation

--- OrderItem.java
+++ OrderItem.java
@@ -1,7 +1,6 @@
 public class OrderItem {
     private final IceCreamFlavor flavor;
     private final int scoops;
-
     public OrderItem(IceCreamFlavor flavor, int scoops) {
         if (scoops <= 0) {
             throw new IllegalArgumentException("Number of scoops must be positive");
@@ -9,15 +8,12 @@ public class OrderItem {
         this.flavor = flavor;
         this.scoops = scoops;
     }
-
     public double getPrice() {
         return flavor.getPricePerScoop() * scoops;
     }
-
     public IceCreamFlavor getFlavor() {
         return flavor;
     }
-
     public int getScoops() {
         return scoops;
     }
