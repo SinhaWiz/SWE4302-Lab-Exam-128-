825bf418489668fa32372d28f8097e4d700c74fa
SinhaWiz <sinhartx2070@gmail.com>
2024-11-20 17:16:35 +0600

    TEST cases added

--- Task_2/reportScheduler.java
+++
@@ -1,7 +0,0 @@
-package Task_2;
-
-class reportScheduler {
-    public static void scheduleReport(String reportType, String frequency) {
-        System.out.println("Scheduling " + reportType + " report with frequency " + frequency);
-    }
-}
