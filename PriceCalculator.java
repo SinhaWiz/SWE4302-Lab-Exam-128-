8389b1512fb6ee53b4720da8c4db6ed30ad438c8
SinhaWiz <sinhartx2070@gmail.com>
2024-11-20 15:42:42 +0600

    added method for price calculation

---
+++ PriceCalculator.java
@@ -0,0 +1,24 @@
+
+public class PriceCalculator {
+    private static final double TAX_RATE = 0.08;
+    public double calculateSubtotal(Order order) {
+        double subtotal = order.getContainer().getPrice();
+
+        for (OrderItem item : order.getIceCreamItems()) {
+            subtotal += item.getPrice();
+        }
+
+        for (ToppingItem topping : order.getToppingItems()) {
+            subtotal += topping.getPrice();
+        }
+        return subtotal;
+    }
+
+    public double calculateTax(double subtotal) {
+        return subtotal * TAX_RATE;
+    }
+
+    public double calculateTotal(double subtotal, double tax) {
+        return subtotal + tax;
+    }
+}
